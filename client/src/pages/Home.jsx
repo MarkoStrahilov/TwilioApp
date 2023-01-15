ea9ee4522124fd8152f1f4c1f2736651acba3d53
Marko Strahilov <strahilovmarko@yahoo.com>
2023-01-16 00:53:01 +0100

    update add-home page

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Hero from './Hero';
+import Features from './Features';
+import Testimontials from './Testimontials';
+import ThreeTierPricingHorizontal from './PricingFeature';
+import NewsLetter from './Newsletter';
+import Footer from './Footer'
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar />
+        <Hero />
+        <Features />
+        <Testimontials />
+        <ThreeTierPricingHorizontal />
+        <NewsLetter />
+        <Footer />
+    </div>
+  )
+}
+
+export default Home
