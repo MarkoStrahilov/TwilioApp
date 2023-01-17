a4eae858f23897989b4239af31e3f940a171ccfc
MarkoStrahilov <72885606+MarkoStrahilov@users.noreply.github.com>
2023-01-17 19:12:33 +0100

    add react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
+import { Routes,Route } from 'react-router-dom';
 import Home from './pages/Home';
 import SendMessages from './components/SendMessages';
+import PricingPlans from './components/PricingPlans';
+import Contact from './pages/Contact'
 
 function App() {
 
-  const account = true;
-
   return (
     <div className="App">
-      {account ? <Home /> : <SendMessages />}
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/dashboard' element={<SendMessages />}/>
+        <Route path='/pricing-plans' element={<PricingPlans />}/> 
+        <Route path='/contact' element={<Contact />}/>
+      </Routes>
     </div>
   );
+  
 }
 
 export default App;
--- client/src/components/SendMessages.jsx
+++ client/src/components/SendMessages.jsx
@@ -1,7 +1,5 @@
 import { Stack, HStack, VStack, Box, Heading, Text, Center, Container } from '@chakra-ui/react';
 import { MdCheckCircle } from 'react-icons/md';
-import Nav from '../shared/Nav';
-
 import {
     List,
     ListItem,
@@ -10,6 +8,19 @@ import {
     UnorderedList,
 } from '@chakra-ui/react'
 
+import {
+    FormControl,
+    FormLabel,
+    Input,
+    InputGroup,
+    InputRightElement,
+    Button,
+    Textarea
+} from '@chakra-ui/react';
+
+import { useState } from 'react';
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+import Nav from '../shared/Nav';
 
 function Feature({ title, desc, ...rest }) {
     return (
@@ -21,36 +32,57 @@ function Feature({ title, desc, ...rest }) {
 }
 
 export default function SendMessages() {
+
     return (
         <>
-        <Nav />
+            <Nav />
             <Heading as='h3' size='lg' textAlign={'center'}>
                 Send text messages with our provider
             </Heading>
-                <List spacing={3}>
-                    <ListItem>
-                        <ListIcon as={MdCheckCircle} color='green.500' />
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit
-                    </ListItem>
-                    <ListItem>
-                        <ListIcon as={MdCheckCircle} color='green.500' />
-                        Assumenda, quia temporibus eveniet a libero incidunt suscipit
-                    </ListItem>
-                    <ListItem>
-                        <ListIcon as={MdCheckCircle} color='green.500' />
-                        Quidem, ipsam illum quis sed voluptatum quae eum fugit earum
-                    </ListItem>
-                    {/* You can also use custom icons from react-icons */}
-                    <ListItem>
-                        <ListIcon as={MdCheckCircle} color='green.500' />
-                        Quidem, ipsam illum quis sed voluptatum quae eum fugit earum
-                    </ListItem>
-                </List>
+            <List spacing={3}>
+                <ListItem>
+                    <ListIcon as={MdCheckCircle} color='green.500' />
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit
+                </ListItem>
+                <ListItem>
+                    <ListIcon as={MdCheckCircle} color='green.500' />
+                    Assumenda, quia temporibus eveniet a libero incidunt suscipit
+                </ListItem>
+                <ListItem>
+                    <ListIcon as={MdCheckCircle} color='green.500' />
+                    Quidem, ipsam illum quis sed voluptatum quae eum fugit earum
+                </ListItem>
+                <ListItem>
+                    <ListIcon as={MdCheckCircle} color='green.500' />
+                    Quidem, ipsam illum quis sed voluptatum quae eum fugit earum
+                </ListItem>
+            </List>
             <Center>
                 <HStack spacing={8} mt={"2rem"}>
                     <Box p={5} shadow='md' borderWidth='1px'>
-                        <Heading fontSize='xl'>Inputs</Heading>
+                        <Heading fontSize='xl'>Create Your Message</Heading>
                         <Text mt={4}>here we show inputs that help create the message</Text>
+                        <Box mt={4}>
+                            <FormControl id="sms-subject" isRequired mt={3}>
+                                <FormLabel>SMS Subject</FormLabel>
+                                <Input type="sms-subject" />
+                            </FormControl>
+                            <FormControl id="sms-message" isRequired mt={3}>
+                                <FormLabel>SMS Body</FormLabel>
+                                <FormControl id="sms-message">
+                                    <Textarea
+                                        borderColor="gray.300"
+                                        _hover={{
+                                            borderRadius: 'gray.300',
+                                        }}
+                                        placeholder="message"
+                                    />
+                                </FormControl>
+                            </FormControl>
+                            <div style={{textAlign: "end", marginTop: "2rem"}}>
+                                <Button colorScheme='blue' >Send Message</Button>
+                            </div>
+                        </Box>
                     </Box>
                     <Box p={5} shadow='md' borderWidth='1px' >
                         <Heading fontSize='xl'>Message Showcase</Heading>
